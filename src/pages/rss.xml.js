026b2a26ca833228bea61724133fcb35e1eb0aca
Isaac Blake <blakeearth@users.noreply.github.com>
2025-05-20 22:27:10 -0500

    Cleaned up my RSS feed a little bit

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -8,9 +8,14 @@ export async function GET(context) {
         title: SITE_TITLE,
         description: SITE_DESCRIPTION,
         site: context.site,
-        items: posts.map((post) => ({
-            ...post.data,
-            link: `/cards/${post.id}/`,
-        })),
+        items: posts
+            .sort((a, b) => new Date(b.data.pubDate) - new Date(a.data.pubDate))
+            .map((post) => ({
+                title: post.data.title,
+                description:
+                    post.data.description ??
+                    post.body.substring(0, 499) + "...",
+                link: `/cards/${post.id}/`,
+            })),
     })
 }
