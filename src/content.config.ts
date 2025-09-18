3ddc3089e559c48048ddd660c0c831db0c0d30c4
Isaac Blake <blakeearth@users.noreply.github.com>
2025-09-18 17:43:06 -0500

    add body to collection

--- src/content.config.ts
+++ src/content.config.ts
@@ -7,6 +7,7 @@ const cards = defineCollection({
 		title: z.string(),
 		description: z.string().optional(),
 		pubDate: z.coerce.date(),
+		body: z.string(),
 		updatedDate: z.coerce.date().optional(),
 		heroImage: z.string().optional(),
 	}),
