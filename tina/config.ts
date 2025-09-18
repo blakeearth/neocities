38b03c3982eb14bbee1f91905107668ac16c5cb4
Isaac Blake <blakeearth@users.noreply.github.com>
2025-09-18 13:54:42 -0500

    add tina

---
+++ tina/config.ts
@@ -0,0 +1,49 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch =
+  process.env.GITHUB_BRANCH ||
+  process.env.VERCEL_GIT_COMMIT_REF ||
+  process.env.HEAD ||
+  "main";
+
+export default defineConfig({
+  branch,
+  clientId: "fe460813-9c0d-48ad-b3d5-1cb7700fae99", // Get this from tina.io
+  token: null, // Get this from tina.io
+  build: {
+    outputFolder: "dist",
+    publicFolder: "public",
+  },
+  media: {
+    tina: {
+      mediaRoot: "public/images",
+      publicFolder: "public",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        name: "cards",
+        label: "Cards",
+        path: "src/content/cards",
+        format: 'mdx',
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "datetime",
+            name: "pubDate",
+            label: "Date Posted",
+            required: true,
+          },
+        ],
+      },
+    ],
+  },
+});
